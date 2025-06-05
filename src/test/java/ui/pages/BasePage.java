e0c0804dbadf0dd2dbc81e074e55352799010fa5
TQ6 <51542960+TQ-auto@users.noreply.github.com>
2025-06-05 14:36:55 +0300

    Change host ip

--- src/test/java/ui/pages/BasePage.java
+++ src/test/java/ui/pages/BasePage.java
@@ -1,5 +1,6 @@
 package ui.pages;
 
+import api.endpoints.Routes;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -8,7 +9,7 @@ public abstract class BasePage {
 
     WebDriver driver;
     WebDriverWait webdriverWait;
-    protected final String baseUrl = "http://adminjs-app:3000/admin";
+    protected final String baseUrl = Routes.HOST + "/admin";
 
     protected BasePage(WebDriver driver){
         this.driver = driver;
